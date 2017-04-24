2b1bfd108c71b0af56996534b880e1419e7427e0
Michael Jabbour <micjabbour@gmail.com>
2017-04-25 01:03:41 +0300

    WIP: some progress writing documentation
    add update-docs.bat batch file to generate documentation from sources.
    edit doxygen configuration file. write some documentation in QSqlQuery
    class.

--- msqlquery-demo/msqlquery/msqldatabase.h
+++ msqlquery-demo/msqlquery/msqldatabase.h
@@ -10,31 +10,32 @@ class MSqlThread;
 /*!
  * \brief The MSqlDatabase class represents a database connection that can be
  *        used from MSqlQuery.
- * 
+ *
  * The class provides an interface similar to QSqlDatabase. Create a connection
  * by calling the static addDatabase() function, This connection can afterwards
- * be used from MSqlQuery. If you don't pass the connection name argument, 
+ * be used from MSqlQuery. If you don't pass the connection name argument,
  * MSqlQuery's default connection is assumed (this is different from QSqlQuery's
  * default connection). The following snippet shows how to create and open a
- * default MSqlQuery connection to a PostgreSQL database:
- * 
+ * default MSqlQuery connection to a PostgreSQL database (same as QSqlDatabase
+ * example):
+ *
  *     MSqlDatabase db = MSqlDatabase::addDatabase("QPSQL");
  *     db.setHostName("acidalia");
  *     db.setDatabaseName("customdb");
  *     db.setUserName("mojito");
  *     db.setPassword("J0a1m8");
  *     bool ok = db.open();
- * 
+ *
  * The class supports all connection types that are supported in QSqlDatabase.
- * 
+ *
  * Internally, the class sets up a QSqlDatabase connection and a QThread, and
  * ensures that all accesses to that connection is done from its reserved
  * QThread.
- * 
+ *
  * \warning Connections that are set up using MSqlDatabase should be used only
  *          from MSqlQuery classes (So that MSqlQuery can ensure that calls are
  *          done in the right thread).
- * 
+ *
  * \warning Connections that are set up using QSqlDatabase should be used only
  *          from QSqlQuery classes (the caller is responsible to ensure that a
  *          connection is accessed only from the thread that created it).
@@ -46,13 +47,13 @@ public:
     ~MSqlDatabase();
     /*!
      * \brief Similar to QSqlDatabase::addDatabase.
-     * 
+     *
      * Adds a database to the list of database connections using the supplied
      * driver type and the connectionName. Internally, The database connection
      * is created in a background thread managed by the library. When using the
      * library to execute queries on that database connection, all calls are
      * internally done in the connection's background thread.
-     * 
+     *
      * \param type a registered driver type
      * \param connectionName
      * \return an uninitialized MSqlDatabase object that controls the added
@@ -62,73 +63,73 @@ public:
                                     const QString& connectionName = defaultConnectionName);
     /*!
      * \brief Similar to QSqlDatabase::database.
-     * 
+     *
      * Returns the database connection called *connectionName*. If no
      * connectionName is specified, MSqlQuery's default connection is used.
-     * 
+     *
      * \param connectionName
      * \return an MSqlDatabase object that controls the specified connection.
      */
     static MSqlDatabase database(const QString& connectionName = defaultConnectionName);
     /*!
      * \brief Similar to QSqlDatabase::setHostName.
-     * 
+     *
      * Sets the connection's host name to *host*.
-     * 
+     *
      * \param host new host name.
      */
     void setHostName(const QString& host);
     /*!
      * \brief Similar to QSqlDatabase::setDatabaseName.
-     * 
+     *
      * Sets the connection's database name to *name*.
-     * 
-     * \param name database name 
+     *
+     * \param name database name
      */
     void setDatabaseName(const QString& name);
     /*!
      * \brief Similar to QSqlDatabase::setUserName.
-     * 
+     *
      * Sets the connection's user name to *name*.
-     * 
+     *
      * \param name username
      */
     void setUserName(const QString& name);
     /*!
      * \brief Similar to QSqlDatabase::setPassword.
-     * 
+     *
      * Sets the connection's password to *password*.
-     * 
+     *
      * \param password password
      */
     void setPassword(const QString& password);
     /*!
      * \brief Similar to QSqlDatabase::setConnectOptions.
-     * 
+     *
      * Sets database-specific options.
-     * 
+     *
      * \param options a QString of a semicolon separated list of option names or
      *                option=value pairs.
      */
     void setConnectOptions(const QString& options = QString());
     /*!
      * \brief Similar to QSqlDatabase::setPort
-     * 
+     *
      * Sets the connection's port number to *port*.
-     * 
+     *
      * \param port port.
      */
     void setPort(int port);
     /*!
      * \brief Similar to QSqlDatabase::connectionName.
-     * 
+     *
      * \return the connection name.
      */
     QString connectionName()const{return m_connectionName;}
     /*!
      * \brief Similar to QSqlDatabase::hostName.
      * \return the connection's host name.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::hostName in
      *          the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
@@ -139,7 +140,7 @@ public:
     /*!
      * \brief Similar to QSqlDatabase::databaseName.
      * \return the connection's database name.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::databaseName
      *          in the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
@@ -150,7 +151,7 @@ public:
     /*!
      * \brief Similar to QSqlDatabase::userName.
      * \return the connection's username.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::userName in
      *          the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
@@ -161,7 +162,7 @@ public:
     /*!
      * \brief Similar to QSqlDatabase::password.
      * \return the connection's password.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::password in
      *          the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
@@ -172,7 +173,7 @@ public:
     /*!
      * \brief Similar to QSqlDatabase::connectOptions.
      * \return the connection options used for this connection.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::connectOptions
      *          in the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
@@ -182,58 +183,58 @@ public:
     QString connectOptions()const;
     /*!
      * \brief Similar to QSqlDatabase::transaction.
-     * 
+     *
      * Begins a transaction on the database if the driver supports transactions.
-     * 
+     *
      * \return true if the operation succeeded. Otherwise it returns false.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::transaction
      *          in the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
      *          (executing a query for example), this function might block the
      *          calling thread until the ongoing query finishes.
-     * 
+     *
      * \sa commit(), rollback()
      */
     bool transaction();
     /*!
      * \brief Similar to QSqlDatabase::commit.
-     * 
-     * Commits a transaction to the database of thee driver supports 
+     *
+     * Commits a transaction to the database of thee driver supports
      * transactions and a transation() has been started.
-     * 
+     *
      * \return true if the operation succeeded. Otherwise it returns false.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::commit in
      *          the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
      *          (executing a query for example), this function might block the
      *          calling thread until the ongoing query finishes.
-     * 
+     *
      * \sa transaction(), rollback()
      */
     bool commit();
     /*!
      * \brief Similar to QSqlDatabase::rollback.
-     * 
+     *
      * Rolls back a transaction on the database if the driver supports
      * transactions and a transaction() has been started.
-     * 
+     *
      * \return true if the operation succeeded. Otherwisse it returns false.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::rollback in
      *          the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
      *          (executing a query for example), this function might block the
      *          calling thread until the ongoing query finishes.
-     * 
+     *
      * \sa transaction(), commit()
      */
     bool rollback();
     /*!
      * \brief Similar to QSqlDatabase::port.
      * \return the connection's port number.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::port in
      *          the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
@@ -244,24 +245,24 @@ public:
     /*!
      * \brief Similar to QSqlDatabase::lastError.
      * \return information about the last error that occured on the database.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::lastError
      *          in the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
      *          (executing a query for example), this function might block the
      *          calling thread until the ongoing query finishes.
      *          Use MSqlQuery::lastError for a non-blocking solution.
-     * 
+     *
      * \sa MSqlQuery::lastError
      */
     QSqlError lastError()const;
     /*!
      * \brief Similar to QSqlDatabase::open
-     * 
+     *
      * Opens the database connection using the current connection values.
-     * 
+     *
      * \return true on success. Otherwise, it returns false.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::open in the
      *          database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
@@ -271,10 +272,10 @@ public:
     bool open();
     /*!
      * \brief Similar to QSqlDatabase::close
-     * 
-     * Closes the database connection, freeing any resources acquired, and 
+     *
+     * Closes the database connection, freeing any resources acquired, and
      * invalidating any MSqlQuery objects that are used with the database.
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::close in
      *          the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
@@ -288,20 +289,20 @@ public:
     
     /*!
      * \brief Similar to QSqlDatabase::driver()->%subscribeToNotification()
-     * 
+     *
      * Call this function to subscribe to event notifications from the database.
-     * 
+     *
      * \warning Do not use MSqlDatabase::driver()->%subscribeToNotification(),
      *          (because it will try to access the driver from the current
      *          thread). Instead, call this function.
-     * 
+     *
      * \warning The function is implemented by calling
      *          QSqlDatabase::driver()->%subscribeToNotification() in the
      *          database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
      *          (executing a query for example), this function might block the
      *          calling thread until the ongoing query finishes.
-     * 
+     *
      * \param name identifies the event notification
      * \return true if the operation succeeded. Otherwisse it returns false.
      * \sa subscribedToNotifications(), unsubscribeFromNotification(), driver()
@@ -309,18 +310,18 @@ public:
     bool subscribeToNotification(const QString & name);
     /*!
      * \brief Similar to QSqlDatabase::driver()->%subscribedToNotifications()
-     * 
+     *
      * \warning Do not use MSqlDatabase::driver()->%subscribedToNotifications(),
      *          (because it will try to access the driver from the current
      *          thread). Instead, call this function.
-     * 
+     *
      * \warning The function is implemented by calling
      *          QSqlDatabase::driver()->%subscribedToNotifications() in the
      *          database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
      *          (executing a query for example), this function might block the
      *          calling thread until the ongoing query finishes.
-     * 
+     *
      * \return a list of the names of the event notifications that are currently
      *         subscribed to.
      * \sa subscribeToNotification(), unsubscribeFromNotification(), driver()
@@ -328,11 +329,11 @@ public:
     QStringList subscribedToNotifications()const;
     /*!
      * \brief Similar to QSqlDatabase::driver()->%unsubscribeFromNotification()
-     * 
+     *
      * \warning Do not use MSqlDatabase::driver()->%unsubscribeFromNotification,
      *          (because it will try to access the driver from the current
      *          thread). Instead, call this function.
-     * 
+     *
      * \warning The function is implemented by calling
      *          QSqlDatabase::driver()->%unsubscribeFromNotification() in the
      *          database connection's thread and returning its result.
@@ -346,52 +347,52 @@ public:
     bool unsubscribeFromNotification(const QString& name);
     /*!
      * \brief Similar to QSqlDatabase()::driver().
-     * 
+     *
      * \warning The function returns a QSqlDriver object. Do not call any
      *          functions on the returned object. You can only connect to
      *          signals being emitted from this object (notification() signal).
-     * 
-     * \return the database driver used to access the database connection. Any 
+     *
+     * \return the database driver used to access the database connection. Any
      *         function call on the returned object may cause undefined behavior
      * \sa subscribeToNotification(), subscribedToNotifications(),
      *     unsubscribeFromNotification()
      */
-    const QSqlDriver* driver(); 
+    const QSqlDriver* driver();
     /*!
      * \brief Similar to QSqlDatabase::isOpen()
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::isOpen in
      *          the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
      *          (executing a query for example), this function might block the
      *          calling thread until the ongoing query finishes.
-     * 
+     *
      * \return true if the database connection is currently open. Otherwise
      *         returns false
      */
     bool isOpen()const;
     /*!
      * \brief Similar to QSqlDatabase::isOpenError()
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::isOpenError
      *          in the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
      *          (executing a query for example), this function might block the
      *          calling thread until the ongoing query finishes.
-     * 
+     *
      * \return true if there was an error opening the database connection.
      *         Otherwise returns false.
      */
     bool isOpenError()const;
     /*!
      * \brief Similar to QSqlDatabase::isValid()
-     * 
+     *
      * \warning The function is implemented by calling QSqlDatabase::isValid
      *          in the database connection's thread and returning its result.
      *          This means that if the database connection's thread is busy
      *          (executing a query for example), this function might block the
      *          calling thread until the ongoing query finishes.
-     * 
+     *
      * \return true if the QSqlDatabase has a valid driver.
      */
     bool isValid()const;
--- msqlquery-demo/msqlquery/msqlquery.h
+++ msqlquery-demo/msqlquery/msqlquery.h
@@ -12,23 +12,203 @@ class MSqlQueryWorker;
 
 //all functions in this class do NOT block EXCEPT the exec() function
 //use execAsync() and connect to resultsReady() signal instead
+
+/*!
+ * \brief The MSqlQuery class provides means of executing single SQL statement
+ *        synchronously or asynchronously.
+ *
+ * The class provides a subset of the interface of QSqlQuery, in addition to the
+ * asynchronous functions. It can be used to execute any kind of SQL statement
+ * on a database and get results synchronously or asynchronously. Most of the
+ * functions in the class are just wrappers that call corresponding Qt functions
+ * in the right thread.
+ *
+ * The class has a synchronous API, that can be used in the same way QSqlQuery
+ * is used. The following snippet shows how to execute an SQL query and retrieve
+ * results synchronously using MSqlQuery (same as QSqlQuery example):
+ *
+ *     MSqlQuery query("SELECT country FROM artist");
+ *     while (query.next()) {
+ *         QString country = query.value(0).toString();
+ *         doSomething(country);
+ *     }
+ *
+ * The class also has an asynchronous API, that can be used to execute a query
+ * asynchronously. It works by calling functions that end with `Async()` (e.g.
+ * execAsync(), execBatchAsync()). These functions emit a signal resultsReady()
+ * when execution is finished. You can connect to this signal, and access the
+ * results the same way as when using the synchronous API. Here is the same
+ * example above written asynchronously:
+ *
+ *     //create an MSqlQuery object, and provide some parent to manage its lifetime
+ *     MSqlQuery* query = new MSqlQuery(parent);
+ *     //connect to resultsReady signal
+ *     QObject::connect(&query, &MSqlQuery::resultsReady, [=](bool success){
+ *         if(success) {
+ *     	    while (query->next()) {
+ *             QString country = query->value(0).toString();
+ *             doSomething(country);
+ *     	} else {
+ *     	    //statement failed to execute
+ *     		qDebug() << query->lastError().databaseText();
+ *     	}
+ *     });
+ *     //make sure to keep the lifetime of the MSqlQuery object as long as you
+ *     //don't want to cancel the ongoing operation
+ *
+ * The class also supports using QSqlQuery's batch execution mode. This can be
+ * done using the functions execBatch (for synchronous execution) and
+ * execBatchAsync (for asynchronous execution)
+ */
 class MSqlQuery : public QObject {
     Q_OBJECT
 public:
+    /*!
+     * \brief constructs a new %MSqlQuery object.
+     *
+     * The new object uses the supplied MSqlDatabase connection
+     *
+     * \param parent parent QObject
+     * \param db the MSqlDatabase connection to use
+     */
     explicit MSqlQuery(QObject *parent = 0, MSqlDatabase db = MSqlDatabase::database());
+
+    /*!
+     * \brief destroys the %MSqlQuery object.
+     *
+     * The destructor will try to cancel any ongoing asynchronous executions
+     */
     ~MSqlQuery();
-    //an interface similar to QSqlQuery
+
+    /*!
+     * \brief Similar to QSqlQuery::prepare.
+     *
+     * The function prepares an SQL statement for execution.
+     *
+     * \note The function is fully asynchronous
+     *
+     * \warning Unlike QSqlQuery::prepare, this function does not report
+     *          success or failure. This is by design (in order to make the
+     *          function fully asynchronous). You can get failure/success status
+     *          when executing the statement (using the resultsReady signal).
+     *
+     * \param query query statment to prepare
+     */
     void prepare(const QString& query);
+
+    /*!
+     * \brief Similar to QSqlQuery::addBindValue.
+     *
+     * Adds the value *val* to the list of values when using positional binding.
+     *
+     * \note The function is fully asynchronous
+     *
+     * \param val value to bind
+     * \param paramType can be QSql::In, QSql::Out, or QSql::InOut
+     */
     void addBindValue(const QVariant& val, QSql::ParamType paramType = QSql::In);
+    /*!
+     * \brief Similar to QSqlQuery::bindValue.
+     *
+     * Binds the value *val* to the placeholder *placeholder*.
+     *
+     * \note The function is fully asynchronous
+     *
+     * \param placeholder the placeholder to bind the value to
+     * \param val the value that needs to be bound
+     * \param paramType can be QSql::In, QSql::Out, or QSql::InOut
+     */
     void bindValue(const QString& placeholder, const QVariant& val, QSql::ParamType paramType = QSql::In);
+
+    /*!
+     * \brief Similar to QSqlQuery::exec(const QString& query).
+     *
+     * Executes the SQL statement in *query* synchronously.
+     *
+     * \note The function blocks the calling thread until execution is finished.
+     *
+     * \param query the SQL statement to execute
+     * \return true on success, false otherwise.
+     * \sa execAsync(const QString& query).
+     */
     bool exec(const QString& query);
+
+    /*!
+     * \brief Similar to QSqlQuery::exec().
+     *
+     * Executes a previously prepared SQL statement.
+     *
+     * \note The function blocks the calling thread until execution is finished.
+     *
+     * \return true on success, false otherwise.
+     * \sa execAsync()
+     */
     bool exec();
+    /*!
+     * \brief Similar to QSqlQuery::execBatch
+     *
+     * Executes a previously prepared SQL query in a batch.
+     *
+     * \note The function blocks the calling thread until execution is finished.
+     *
+     * \param mode specifies batch execution mode (similar to QSqlQuery)
+     * \return true on success, false otherwise.
+     */
     bool execBatch(QSqlQuery::BatchExecutionMode mode = QSqlQuery::ValuesAsRows);
+    /*!
+     * \brief Similar to QSqlQuery::next
+     *
+     * Positions the query on the next record
+     * \return true on success, false otherwise.
+     *
+     * \note The function is fully asynchronous
+     *
+     * \warning When using any of %MSqlQuery's exec functions, execution is not
+     *          finished until all queries are retrieved. This might be slower
+     *          in some situations, but it is by design (in order to make
+     *          functions like this fully  asynchronous).
+     */
     bool next();
+    /*!
+     * \brief Similar to QSqlQuery::seek
+     * Positions the query on the record at position *index*
+     * \param index record index
+     * \return true on success, false otherwise.
+     *
+     * \note The function is fully asynchronous
+     *
+     * \warning When using any of %MSqlQuery's exec functions, execution is not
+     *          finished until all queries are retrieved. This might be slower
+     *          in some situations, but it is by design (in order to make
+     *          functions like this fully  asynchronous).
+     */
     bool seek(int index);
-    QSqlRecord record() const;
+    /*!
+     * \brief Similar to QSqlQuery::record
+     *
+     * \return a `QSqlRecord` that contains the current row's values.
+     *
+     * \note The function is fully asynchronous
+     *
+     * \warning When using any of %MSqlQuery's exec functions, execution is not
+     *          finished until all queries are retrieved. This might be slower
+     *          in some situations, but it is by design (in order to make
+     *          functions like this fully  asynchronous).
+     */
+    QSqlRecord record() const; //TODO: add value() function
+    /*!
+     * \brief lastError
+     * \return
+     */
     QSqlError lastError() const;
+    /*!
+     * \brief execAsync
+     * \param query
+     */
     void execAsync(const QString& query);
+    /*!
+     * \brief execAsync
+     */
     void execAsync();
     void execBatchAsync(QSqlQuery::BatchExecutionMode mode = QSqlQuery::ValuesAsRows);
     QString getDbConnectionName()const{return db.connectionName();}
@@ -90,7 +270,7 @@ private:
         //every query to be executed has an id, so that it can be overwritten later
         //when a query is finished, its id is checked to make sure that it has not been overwritten
         //(in order to emit resultsReady signal only for the last query set on this object)
-        int queryId = -1; 
+        int queryId = -1;
         QString prepareStr;
         QList<PlaceHolderBind> placeHolderBinds;
         QList<PositionalBind> positionalBinds;
